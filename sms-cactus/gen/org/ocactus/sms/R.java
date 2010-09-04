64889325a8af78bb15e13d8c2d5d5b03cedd946a
Josef Pfleger <green-carrot@orange-cactus.org>
2010-09-04 17:27:01 +0200

    add settings layout files

--- sms-cactus/gen/org/ocactus/sms/R.java
+++ sms-cactus/gen/org/ocactus/sms/R.java
@@ -14,14 +14,27 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int archiveButton=0x7f050000;
-        public static final int sendButton=0x7f050001;
+        public static final int archiveButton=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int sendButton=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int main=0x7f060000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int settings_serverPwd_summary=0x7f050007;
+        public static final int settings_serverPwd_title=0x7f050006;
+        public static final int settings_serverUrl_summary=0x7f050003;
+        public static final int settings_serverUrl_title=0x7f050002;
+        public static final int settings_serverUsr_summary=0x7f050005;
+        public static final int settings_serverUsr_title=0x7f050004;
+        public static final int settings_title=0x7f050001;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
---
+++ sms-cactus/src/org/ocactus/sms/Settings.java
@@ -0,0 +1,14 @@
+package org.ocactus.sms;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+
+}
