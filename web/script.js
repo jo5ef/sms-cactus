9b5a80b023c83fce9983303a61f13528aa6c931c
Josef Pfleger <green-carrot@orange-cactus.org>
2010-09-05 14:47:25 +0200

    added character counter

--- web/script.js
+++ web/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	
+	$('textarea').keyup(function() {
+		$('span.count').html(this.value.length);
+	});
 	$.getJSON('msg/latest', function(data) {
 		for(var i = data.length - 1; i >= 0; i--) {
 			var timestamp = new Date(+data[i].timestamp);
